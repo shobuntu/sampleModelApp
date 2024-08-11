74105db117061fc74a28ae72f508ed0333166bbb
SHOBHIT <shobhit1106@gmail.com>
2024-08-12 00:49:19 +0530

    flask changes

--- src/app.py
+++ src/app.py
@@ -1,17 +1,10 @@
 from flask import Flask
-from flask_graphql import GraphQLView
-from schema import schema
 
 app = Flask(__name__)
 
-app.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view(
-        'graphql',
-        schema=schema,
-        graphiql=True  # Enable GraphiQL UI
-    )
-)
+@app.route("/")
+def hello_world():
+    return "Hello, Shobhit"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
